2bee69f6b135036c8e58733cffb3a7368ed46f49
Mirza Athallah Salman <atar.haryanto@gmail.com>
2024-09-17 13:16:41 +0700

    tugas 3 done

--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
+import uuid
 from django.db import models
 
-class MoodEntry(models.Model):
+class ProductEntry(models.Model):
     name = models.CharField(max_length=255)
-    price = models.FloatField(auto_now_add=True)
+    price = models.FloatField()
     description = models.TextField()
